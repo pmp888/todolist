a867b8b62f2f319f79c70e87520c5ab134538d67
Pedro Miguel <pedrom888@gmail.com>
2019-04-09 20:14:38 +0700

    change addTodo as object, add Boolean

--- HMTLday/todolist/script.js
+++ HMTLday/todolist/script.js
@@ -1,19 +1,22 @@
 var todoList = {
-      todos: ['item1', 'item2', 'item3'], //todos array
+      todos: [], //todos array
 
       displayTodos: function() {
         console.log ('My Todos: ', this.todos)
         },
 
-      addTodos: function(add) {
-        this.todos.push (add);
+      addTodo: function(todoText) {
+        this.todos.push({
+          todoText: todoText,
+          completed: false
+        });
         this.displayTodos();
         },
-      changeTodos: function(position, newValue) {
+      changeTodo: function(position, newValue) {
         this.todos[position] = newValue;
         this.displayTodos();
         },
-      deleteTodos: function (position) {
+      deleteTodo: function (position) {
         this.todos.splice (position, 1);
         this.displayTodos();
       }
