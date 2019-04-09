7eaa6147e090405eaea407c0f9d89a2496d27e6b
Pedro Miguel <pedrom888@gmail.com>
2019-04-09 19:54:09 +0700

    add deleteTodos , organized code layout

--- HMTLday/todolist/script.js
+++ HMTLday/todolist/script.js
@@ -1,15 +1,20 @@
 var todoList = {
-  todos: ['item1', 'item2', 'item3'], //todos array
-  displayTodos: function() {
-    console.log ('My Todos: ', this.todos)
-  },
-  addTodos: function(add) {
-    this.todos.push (add);
-    this.displayTodos()
-  },
-  changeTodos: function(position, newValue) {
-    this.todos[position] = newValue;
-    this.displayTodos()
-  },
-  
-};
+      todos: ['item1', 'item2', 'item3'], //todos array
+
+      displayTodos: function() {
+        console.log ('My Todos: ', this.todos)
+        },
+
+      addTodos: function(add) {
+        this.todos.push (add);
+        this.displayTodos();
+        },
+      changeTodos: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+        },
+      deleteTodos: function (position) {
+        this.todos.splice (position, 1);
+        this.displayTodos();
+      }
+    };
