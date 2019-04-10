837f433f1f7876d5defc710e4cd9d8f5801441d6
Pedro Miguel <pedrom888@gmail.com>
2019-04-10 17:49:24 +0700

    refactoring with var handlers

--- HMTLday/todolist/script.js
+++ HMTLday/todolist/script.js
@@ -5,15 +5,16 @@ var todoList = {
         if (this.todos.length === 0) {
           console.log ('Todo List is empty')
         }
+
         else {
           console.log ('My Todos (' + this.todos.length + '): ');
           for (var i = 0; i < this.todos.length; i++) {
-          if (this.todos[i].completed === true) {
-          console.log ('[X]', this.todos[i].todoText);
-        }
-        else {
-          console.log ('[ ]', this.todos[i].todoText);
-              }
+                if (this.todos[i].completed === true) {
+                  console.log ('[X]', this.todos[i].todoText);
+                }
+                else {
+                  console.log ('[ ]', this.todos[i].todoText);
+                  }
             }
           }
         },
@@ -69,13 +70,23 @@ var todoList = {
         }
       };
 
-//Grabbing ID and adding Event Listener to DisplayTodos
-      var displayTodosButton = document.getElementById('displayTodosButton');
-      displayTodosButton.addEventListener('click',function(){
-        todoList.displayTodos();
-      });
+// //Grabbing ID and adding Event Listener to DisplayTodos
+//       var displayTodosButton = document.getElementById('displayTodosButton');
+//       displayTodosButton.addEventListener('click',function(){
+//         todoList.displayTodos();
+//       });
+//
+//       var toggleAllButton = document.getElementById('toggleAllButton');
+//       toggleAllButton.addEventListener('click', function(){
+//         todoList.toggleAll();
+//       });
 
-      var toggleAllButton = document.getElementById('toggleAllButton');
-      toggleAllButton.addEventListener('click', function(){
-        todoList.toggleAll();
-      });
+
+var handlers = {
+  displayTodos: function() {
+    todoList.displayTodos();
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
+  }
+}
