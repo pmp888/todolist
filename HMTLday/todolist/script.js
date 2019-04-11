87ea84426c71bd068640a4e4b375eb4c6c519fe2
Pedro Miguel <pedrom888@gmail.com>
2019-04-11 15:47:14 +0700

    all functions are on DOM

--- HMTLday/todolist/script.js
+++ HMTLday/todolist/script.js
@@ -90,9 +90,6 @@ var handlers = {
   displayTodos: function() {
     todoList.displayTodos();
   },
-  toggleAll: function() {
-    todoList.toggleAll();
-  },
   addTodo: function() {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo (addTodoTextInput.value);
@@ -110,6 +107,15 @@ var handlers = {
   deleteTodo: function() {
   var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
   todoList.deleteTodo (deleteTodoPositionInput.value);
+  deleteTodoPositionInput.value = '';
+  },
+  toggleCompleted: function() {
+    var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+    todoList.toggleCompleted (toggleCompletedPositionInput.value);
+    toggleCompletedPositionInput.value = '';
+  },
+  toggleAll: function() {
+    todoList.toggleAll();
   }
 };
 
